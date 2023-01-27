ca36085c2fd07e5209208353499a643c89c1d36b
Ultra-tech-code <hizick27@gmail.com>
2023-01-27 13:29:09 +0100

    offer script works perfectly with the correct output

--- scripts/offer.ts
+++ scripts/offer.ts
@@ -56,10 +56,10 @@ async function main() {
 
   /*********************Deploy byteGetter*************************** */
   const byteGetter = await ethers.getContractFactory("byteGenerator");
-  const ByteGetter = await byteGetter.deploy(forwarder.address, tWRegistry.address);
+  const ByteGetter = await byteGetter.deploy();
   
   await ByteGetter.deployed();
-  console.log(`TWFactory  contract is deployed to ${ByteGetter.address}`);
+  console.log(`ByteGetter contract is deployed to ${ByteGetter.address}`);
  
   //varaiables
 
@@ -68,11 +68,9 @@ async function main() {
   const platformFee = 500; //5%
    /*************INTeract*********** */
   const ByteGetterInteract = byteGetter.attach(ByteGetter.address);
-  const getBytes = await ByteGetterInteract.callStatic.getBytes("MArketplace");
+  const getBytes = await ByteGetterInteract.callStatic.getBytes("Marketplace");
 
   const getEncodeDate = await ByteGetterInteract.callStatic.getEncodeCall(deployer.address, contractURi, [forwarder.address], deployer.address, platformFee)
-
-    
 
   /*****************************Twfactory interact************************* */
   const TWFactoryInteract = TWFactory.attach(tWFactory.address)
@@ -81,15 +79,19 @@ async function main() {
 
   const deployProxy = await TWFactoryInteract.deployProxy(getBytes, getEncodeDate);
 
+   
   const result = await deployProxy.wait()
   //@ts-ignore
-  const proxyMarketplaceAddress = result.events[0].topics;
+ const proxyMarketplaceAddress = result.events[0].args;   
 
-  console.log("Marketplace proxy address: ", proxyMarketplaceAddress) 
+//@ts-ignore
+console.log("marketplace proxy ADdress", proxyMarketplaceAddress[1])
+//@ts-ignore
+const marketplaceAddress = proxyMarketplaceAddress[1];
 
 /************************Interact with the marketrplace******************** */
 const NftMarketplace =  await ethers.getContractFactory("Marketplace")
-const nftMarketplace = NftMarketplace.attach(proxyMarketplaceAddress)
+const nftMarketplace = NftMarketplace.attach(marketplaceAddress)
 
 
 /************************** CreateDummy NFT and TestToken******************************** */
@@ -98,7 +100,7 @@ const testNft = await TestNft.deploy();
 
 await testNft.deployed();
 
-console.log(`Marketplace contract is deployed to ${testNft.address}`);
+console.log(`NFT contract is deployed to ${testNft.address}`);
 
 /*********************************** */
 const TestToken = await ethers.getContractFactory("TestToken");
@@ -106,40 +108,40 @@ const testToken = await TestToken.deploy();
 
 await testToken.deployed();
 
-console.log(`Marketplace contract is deployed to ${testToken.address}`);
+console.log(`TESTToken contract is deployed to ${testToken.address}`);
 
 
 /************************Minting and approval************* */
 const TestNftInteract = TestNft.attach(testNft.address)
-              await TestNftInteract.safeMint(tester1.address)
-const mint = await TestNftInteract.safeMint(tester3.address)
 
-                    await TestNftInteract.connect(tester1).setApprovalForAll(proxyMarketplaceAddress, true)
-const nftApproval = await TestNftInteract.connect(tester3).setApprovalForAll(proxyMarketplaceAddress, true)
+const mint =  await TestNftInteract.safeMint(tester1.address)
+const nftApproval = await TestNftInteract.connect(tester1).setApprovalForAll(marketplaceAddress, true)
 
 
 console.log("approval succesfull ", nftApproval)
 
 /******************** */
 const amt = ethers.utils.parseEther("40")
-const TestTokenInteract = TestToken.attach(testToken.address)
+// const TestTokenInteract = TestToken.attach(testToken.address)
 
-const mintToken = await TestTokenInteract.mint(tester2.address, amt)
+// const mintToken = await TestTokenInteract.mint(tester2.address, amt)
 
+// const tokenApproval = await TestTokenInteract.connect(tester2).approve(marketplaceAddress, amt)
 
-const tokenApproval = await TestTokenInteract.connect(tester2).approve(proxyMarketplaceAddress, amt)
-
-console.log("token approval ", tokenApproval)
+// console.log("token approval ", tokenApproval)
 
 
 /****************************************************************************888 */
 /********************create listing and offer listing method***************8 */
 
 /********** */
+const currentTime = (await ethers.provider.getBlock("latest")).timestamp
+console.log("latest ", currentTime)
+
 const listingParams2 = {
   assetContract: testNft.address,
-  tokenId: 1,
-  startTime: Date.now(),
+  tokenId: 0,
+  startTime: currentTime,
   secondsUntilEndTime: 3600,
   quantityToList: 1,
   currencyToAccept: testToken.address,
@@ -147,7 +149,7 @@ const listingParams2 = {
   buyoutPricePerToken: ethers.utils.parseEther("20"),
   listingType: 0
 }
-const c8list2 = await nftMarketplace.connect(tester3).createListing(listingParams2)
+const c8list2 = await nftMarketplace.connect(tester1).createListing(listingParams2)
 console.log("create listen 2 successfull ", c8list2)
 
 
@@ -158,48 +160,50 @@ const testToken2 = await TestToken2.deploy();
 
 await testToken2.deployed();
 
-console.log(`Marketplace contract is deployed to ${testToken2.address}`);
+console.log(`testToken2 contract is deployed to ${testToken2.address}`);
 
 /********** */
 const TestTokenInteract2 = TestToken2.attach(testToken2.address)
 const mint2Token = await TestTokenInteract2.mint(tester4.address, amt)
 
-const token2Approval = await TestTokenInteract2.connect(tester4).approve(proxyMarketplaceAddress, amt)
+const token2Approval = await TestTokenInteract2.connect(tester4).approve(marketplaceAddress, amt)
 
 console.log("token 2 approval", token2Approval)
 
 /*************offer*******8 */
 //offer params
-const listingId2 = 2 ;
+const listingid = 0 ;
 const quantityWanted = 1;
 const currency2 = testToken2.address;
 const pricePerToken = ethers.utils.parseEther("15");
 const expirationTimestamp = Date.now() + 6400;
 
 
-const c8offer = await nftMarketplace.connect(tester4).offer(listingId2, quantityWanted, currency2, pricePerToken, expirationTimestamp)
+const c8offer = await nftMarketplace.connect(tester4).offer(listingid, quantityWanted, currency2, pricePerToken, expirationTimestamp)
 console.log("offer created successfully ", c8offer)
 
 /**********************Accept offer******************8 */
 const offeror = tester4.address;
-const acceptOffer = await nftMarketplace.connect(tester3).acceptOffer(listingId2, offeror, currency2, pricePerToken)
+const acceptOffer = await nftMarketplace.connect(tester1).acceptOffer(listingid, offeror, currency2, pricePerToken)
 
 console.log("offer accepted successfully ", acceptOffer)
 
 
 /**************Balance Check*********************** */
+console.log("all adrresses ", deployer.address, " tester1",  tester1.address, "tester2", tester2.address, "tester3", tester3.address, "tester4", tester4.address)
 
-const tester3bal = await TestTokenInteract2.connect(tester3).callStatic.balanceOf(tester3.address)
+const tester1bal = await TestTokenInteract2.connect(tester1).callStatic.balanceOf(tester1.address)
 
-console.log("balance of nft owner ", tester3bal)
+console.log("token balance of nft owner ", tester1bal)
 //should get the money 
 
 /*********** */
-
+const platformFeeRecipientbal = await TestTokenInteract2.connect(deployer).callStatic.balanceOf(deployer.address)
 const nftbal4 = await TestNftInteract.connect(tester4).callStatic.balanceOf(tester4.address)
-const nftowner4 = await TestNftInteract.connect(tester4).callStatic.ownerOf(1)
+const nftowner4 = await TestNftInteract.connect(tester4).callStatic.ownerOf(0)
 //balance should increase by 1 and nftowner should be tester4 addr
 
+console.log("Platfrom Fee Recipient Balance ", platformFeeRecipientbal)
 console.log("balance of tester 4: ", nftbal4)
 console.log("nft owner of token id 1: ", nftowner4)
 
